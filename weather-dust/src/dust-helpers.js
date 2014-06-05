9945949e2d7668b452c347eb76fd7ba14dbe9147
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-05 16:13:59 -0600

    Use installed button component instead

--- weather-dust/src/dust-helpers.js
+++ weather-dust/src/dust-helpers.js
@@ -5,8 +5,8 @@ exports.registerHelpers = function() {
     require('raptor-taglib-async/dust').registerHelpers(dust);
     require('raptor-taglib-layout/dust').registerHelpers(dust);
     require('raptor-widgets/dust').registerHelpers(dust);
+    require('raptor-sample-ui-components/dust').registerHelpers(dust);
     raptorDust.registerHelpers({
-        'app-button': require('./components/app-button/renderer'),
         'app-weather': require('./components/app-weather/renderer'),
         'app-choose-location': require('./components/app-choose-location/renderer'),
         'app-current-conditions': require('./components/app-current-conditions/renderer'),
