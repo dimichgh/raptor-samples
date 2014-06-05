cc90d1422bdfc3a3d4a8f9a55bc788aca88774d4
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-05 11:18:25 -0600

    Register view-engine-raptor

--- weather-dust/src/pages/index/client-init.js
+++ weather-dust/src/pages/index/client-init.js
@@ -1,7 +1,9 @@
 var viewEngine = require('view-engine');
 var viewEngineDust = require('view-engine-dust');
 var dust = viewEngineDust.dust;
+
 viewEngine.register('dust', viewEngineDust);
+viewEngine.register('rhtml', require('view-engine-raptor'));
+
 require('../../dust-helpers').registerHelpers(dust);
 require('raptor-widgets').initAllWidgets();
-
