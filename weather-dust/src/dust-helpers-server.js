9fc6f31cf67fb4042212a2e4729321ba60eb7c91
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-05 22:10:22 -0600

    Register browser-refresh-taglib helpers

--- weather-dust/src/dust-helpers-server.js
+++ weather-dust/src/dust-helpers-server.js
@@ -2,4 +2,5 @@ var dust = require('view-engine-dust').dust;
 
 exports.registerHelpers = function() {
     require('raptor-optimizer/dust').registerHelpers(dust);
+    require('browser-refresh-taglib/dust').registerHelpers(dust);
 };
