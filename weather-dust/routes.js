f1bf48bd600527322412b9971549a4fd3c018754
Patrick Steele-Idem <pnidem@gmail.com>
2014-06-05 22:10:33 -0600

    Removed extra line

--- weather-dust/routes.js
+++ weather-dust/routes.js
@@ -2,5 +2,4 @@ module.exports = function(app) {
     app.get('/api/weather/:location', require('./src/api/weather'));
     app.get('/weather/:location', require('./src/pages/index'));
     app.get('/', require('./src/pages/index'));
-
 };
